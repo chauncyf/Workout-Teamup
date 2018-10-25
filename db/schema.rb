49b39aabed548f95452474c084f74b9f3b55f815
chauncy <chauncyf@qq.com>
2018-10-25 14:31:21 -0400

    local db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2018_10_24_035712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string "user_name"
+    t.string "email"
+    t.string "potrait_url"
+    t.float "height"
+    t.float "weight"
+    t.text "physical_condition"
+    t.text "motto"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
