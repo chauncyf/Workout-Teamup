9f31b7480b4501e7f0d26a3d42111e937d97f421
515815530@qq.com <515815530@qq.com>
2018-10-26 16:32:38 -0700

    actionCable

--- db/schema.rb
+++ db/schema.rb
@@ -82,8 +82,6 @@ ActiveRecord::Schema.define(version: 2018_10_25_191405) do
 
   create_table "tags", force: :cascade do |t|
     t.string "type_name"
-    t.string "type_name"
-    t.string "type_name"
     t.string "color"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,26 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# User.create(user_name: 'aaaaaaaa', email: 'chauncyf@gdads.com', password: 'fff198654')
-# Tag.create(type_name: 'aaaaaaaa', color: 'green')
+User.create(user_name: 'fcf', email: 'fcf@brandeis.edu', password: '123456789')
+User.create(user_name: 'zxr', email: 'zxr@brandeis.edu', password: '123456789')
+User.create(user_name: 'glm', email: 'glm@brandeis.edu', password: '123456789')
+User.create(user_name: 'admin', email: 'admin@brandeis.edu', password: '123456789')
+User.create(user_name: 'admin2', email: 'admin2@brandeis.edu', password: '123456789')
+
+Tag.create(type_name: 'pullup', color: 'green')
+Tag.create(type_name: 'push', color: 'green')
+Tag.create(type_name: 'pullup', color: 'green')
+Tag.create(type_name: 'pullup', color: 'green')
+Tag.create(type_name: 'pullup', color: 'green')
+Tag.create(type_name: 'pullup', color: 'green')
+Tag.create(type_name: 'pullup', color: 'green')
+Tag.create(type_name: 'pullup', color: 'green')
+Tag.create(type_name: 'pullup', color: 'green')
+Tag.create(type_name: 'pullup', color: 'green')
+Tag.create(type_name: 'pullup', color: 'green')
+
+
+
+
+
+
