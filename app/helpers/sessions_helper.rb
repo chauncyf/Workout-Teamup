aa6d04ef52364cf769050048d755b2368daa1e65
gggllm <515815530@qq.com>
2018-11-04 13:07:53 -0800

    change ajax data format

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,20 +2,33 @@ module SessionsHelper
   # Logs in the given user.
   def log_in(user)
     session[:user_id] = user.id
+    cookies.encrypted[:user_id] = {
+        value: user.id,
+        expires: 1.year.from_now
+    }
+  end
+
+  def current_user_action_cable
+    User.getOne cookies.encrypted[:user_id]
   end
 
   # Returns the current logged-in user (if any).
-  def current_user
-    User.getOne(session[:user_id])
+  def current_user id = session[:user_id]
+    User.getOne id
   end
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
-    !current_user.nil?
+    flag = !current_user.nil?
+    if !flag
+      cookies.delete :user_id # delete the cookie for actioncable authorization
+    end
+    flag
   end
 
   # Logs out the current user.
   def log_out
     session.delete(:user_id)
+    cookies.delete :user_id
   end
 end
