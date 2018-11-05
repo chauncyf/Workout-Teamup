225f555dea4b09b56b19d15952b58d48d8af525a
irenezxr <xiangranzhao@outlook.com>
2018-11-04 21:41:09 -0500

    Modal form for editing user profile and changing password; add gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,7 @@
 module UsersHelper
+  def gravatar_for(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "http://media.minimalist.business/images/customer-profile-template/ideal-customer-profile.jpg"
+    image_tag(gravatar_url, alt: user.user_name, class: "gravatar")
+  end
 end
