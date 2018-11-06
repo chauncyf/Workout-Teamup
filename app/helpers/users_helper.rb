a453815759a2529dc2c34f6afec97b0a60e90b5e
gggllm <515815530@qq.com>
2018-11-06 11:01:13 -0800

    avatar integration with gravatar. only half way to achieve avatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,8 +2,8 @@ module UsersHelper
   def avatar_for(user)
     #gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     #gravatar_url = "http://media.minimalist.business/images/customer-profile-template/ideal-customer-profile.jpg"
-    unless user.avatar.nil?
-      avatar=user.avatar.variant(resize: "100X100")
+    if user.avatar.attached?
+      avatar=user.avatar.variant(resize: "150X150")
     end
     image_tag(avatar || user.gravatar_url, class: "rounded-circle")
   end
