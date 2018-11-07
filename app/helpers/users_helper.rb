34d9e312ab1fb72cbee0cd2b90501c8f182a38d6
irenezxr <xiangranzhao@outlook.com>
2018-11-07 01:01:10 -0500

    Add search bar, user round image at header

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,12 +1,9 @@
 module UsersHelper
   def avatar_for(user)
-    #gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    #gravatar_url = "http://media.minimalist.business/images/customer-profile-template/ideal-customer-profile.jpg"
-
     image_tag(avatar_url(user), class: "rounded-circle")
   end
 
-  def avatar_url user,size="150X150"
+  def avatar_url user,size="100X100"
     if user.avatar.attached?
       avatar = user.avatar.variant(resize: size)
     end
