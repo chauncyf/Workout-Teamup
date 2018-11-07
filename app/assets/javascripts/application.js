e887c5804aef16dbfb1a00bc43195ab6fb680550
gggllm <515815530@qq.com>
2018-11-07 13:39:53 -0800

    avatar integration with gravatar. only half way to achieve avatar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,53 @@
 //= require turbolinks
 //= require_tree .
 
-
+//= require cropper
 $(function () {
     $(document).on("change", "[data-file-refresh]", function (e) {
         $(this).next().html(e.currentTarget.files[0].name)
     })
+
+    $(document).on("change", "[data-image-push-to]", function (e) {
+        var file = e.currentTarget.files[0]
+        var id = $(e.currentTarget).data('image-push-to')
+        var dom = document.getElementById(id)
+        var reader = new FileReader()
+        reader.addEventListener('load', () => {
+            if (dom.cropper) {
+                dom.cropper.replace(reader.result)
+            } else {
+                dom.src = reader.result
+                var cropper = new Cropper(document.getElementById(id), {
+                    aspectRatio: 1,
+                    cropend() {
+                        console.log(11111)
+                        var canvas = cropper.getCroppedCanvas('image/jpeg')
+                        var dataUrl = canvas.toDataURL()
+                        var file = dataURItoBlob(dataUrl)
+                        e.currentTarget.files[0] = file
+                    }
+                })
+            }
+            //document.getElementById(id).style.display='none'
+        })
+        reader.readAsDataURL(file)
+    })
+
 })
+
+function dataURItoBlob(dataURI) {
+// convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+// separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+// write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    return new Blob([ia], {type: mimeString});
+}
