deab279390b506b395b63c63665b84c0e6d147b5
chauncy <chauncyf@qq.com>
2018-11-10 15:17:29 -0500

    add faker seed, font test(db recreate recommend)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
 User.create(user_name: 'fcf', email: 'fcf@brandeis.edu', password: '123456789', identity: 1)
 User.create(user_name: 'zxr', email: 'zxr@brandeis.edu', password: '123456789', identity: 1)
 User.create(user_name: 'glm', email: 'glm@brandeis.edu', password: '123456789', identity: 1)
@@ -13,14 +14,35 @@ User.create(user_name: 'visitor1', email: 'visitor1@brandeis.edu', password: '12
 User.create(user_name: 'visitor1', email: 'visitor1@brandeis.edu', password: '123456789', identity: 2)
 
 # To fix: timezone
-Activity.create(activity_date: '2018-03-11 10:10:00 UTC', place: 'Gosman', content: 'Come and join us!', starter_id: 1, status: 1, theme_color: 'purple')
-Activity.create(activity_date: '2018-03-12 11:20:00 UTC', place: 'Gosman', content: 'Yeah~~~', starter_id: 1, status: 1, theme_color: 'pink')
-Activity.create(activity_date: '2018-03-13 12:30:00 UTC', place: 'Gosman', content: 'This is awesome!', starter_id: 1, status: 1, theme_color: 'blue')
-Activity.create(activity_date: '2018-03-14 13:40:00 UTC', place: 'Gosman', content: 'Cool!', starter_id: 1, status: 1, theme_color: 'cyan')
-Activity.create(activity_date: '2018-03-15 14:50:00 UTC', place: 'Gosman', content: 'Awesome! Amazing!', starter_id: 1, status: 1, theme_color: 'green')
-Activity.create(activity_date: '2018-03-16 16:00:00 UTC', place: 'Gosman', content: 'Come and join us! Amazing!', starter_id: 1, status: 1, theme_color: 'orange')
-Activity.create(activity_date: '2018-03-16 16:00:00 UTC', place: 'Gosman', content: 'Come and join us! Amazing!', starter_id: 1, status: 1, theme_color: 'brown')
-Activity.create(activity_date: '2018-03-16 16:00:00 UTC', place: 'Gosman', content: 'Come and join us! Amazing!', starter_id: 1, status: 1, theme_color: 'grey')
+# Activity.create(activity_date: '2018-03-11 10:10:00 UTC', place: 'Gosman', content: 'Come and join us!', starter_id: 1, status: 1, theme_color: 'purple')
+10.times do
+  Activity.create(activity_date: Faker::Time.forward(30),
+                  place: Faker::Games::Overwatch.location,
+                  content: Faker::MostInterestingManInTheWorld.quote,
+                  theme_color: [:purple, :pink, :blue, :cyan, :green, :orange, :brown, :grey].sample,
+                  starter_id: 1, status: 1)
+end
+10.times do
+  Activity.create(activity_date: Faker::Time.forward(30),
+                  place: Faker::Hobbit.location,
+                  content: Faker::Hobbit.quote,
+                  theme_color: [:purple, :pink, :blue, :cyan, :green, :orange, :brown, :grey].sample,
+                  starter_id: 1, status: 1)
+end
+10.times do
+  Activity.create(activity_date: Faker::Time.forward(30),
+                  place: Faker::Games::Overwatch.location,
+                  content: Faker::FamousLastWords.last_words,
+                  theme_color: [:purple, :pink, :blue, :cyan, :green, :orange, :brown, :grey].sample,
+                  starter_id: 1, status: 1)
+end
+10.times do
+  Activity.create(activity_date: Faker::Time.forward(30),
+                  place: Faker::Games::Overwatch.location,
+                  content: Faker::Games::Overwatch.quote,
+                  theme_color: [:purple, :pink, :blue, :cyan, :green, :orange, :brown, :grey].sample,
+                  starter_id: 1, status: 1)
+end
 
 Tag.create(type_name: 'pullup', color: 'green')
 Tag.create(type_name: 'push', color: 'orange')
@@ -46,12 +68,4 @@ Follow.create(followee_id:5,follower_id:1)
 Follow.create(followee_id:2,follower_id:4)
 
 
-
 Gym.create(name:"gosman",description:"this is gosman gym for brandeis univeristy open time : xxxxx")
-
-
-
-
-
-
-
