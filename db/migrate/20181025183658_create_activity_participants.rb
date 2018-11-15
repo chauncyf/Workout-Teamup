c905824dcf380d53e8d06ca8ef1c08208861d803
gggllm <515815530@qq.com>
2018-11-14 17:07:16 -0800

    basic notification

--- db/migrate/20181025183658_create_activity_participants.rb
+++ db/migrate/20181025183658_create_activity_participants.rb
@@ -1,7 +1,7 @@
 class CreateActivityParticipants < ActiveRecord::Migration[5.2]
   def change
     create_table :activity_participants do |t|
-      t.integer :participant_id
+      t.integer :user_id
       t.integer :activity_id
       t.integer :identity
 
