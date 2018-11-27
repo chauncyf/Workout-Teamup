989e4105e1d6bc5e740c4c3882f65254b9608dfe
gggllm <515815530@qq.com>
2018-11-27 14:38:46 -0800

    add a qrcode controller that has a get method for generating qrcode

--- config/routes.rb
+++ config/routes.rb
@@ -34,4 +34,6 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+
+  get '/getqrcode/:url',to:'qrcodes#get',as: :getQRcodeUrl
 end
