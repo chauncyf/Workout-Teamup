36e829f7da0b5e86cd067b841d36b76475b8d6ef
gggllm <515815530@qq.com>
2018-12-05 11:57:23 -0800

    get most part of the map done. just cannot using helper in activerecord is this a feature or what.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,18 +1,18 @@
 module UsersHelper
   def avatar_for(user, param = {})
-    param[:class] = "rounded-circle avatar"
+    param[:class] = 'rounded-circle avatar'
     image_tag(avatar_url(user), param)
   end
 
-  def avatar_url user, size = "100X100"
+  def avatar_url user, size = '100x100'
     if user.avatar.attached?
       avatar = user.avatar.variant(resize: size)
     end
-    avatar.nil? ? user.gravatar_url : url_for(avatar)
+    avatar.nil? ? user.gravatar_url(size: size.to_s.split(/x/i)[0]) : url_for(avatar)
   end
 
   def map_avatar_url user
-    avatar_url user, "25x25!"
+    avatar_url user, '25x25!'
   end
 
   def activity_starter_avatar(starter_id)
