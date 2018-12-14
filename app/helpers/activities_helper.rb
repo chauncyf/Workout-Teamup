31f5e960569bb3b4e55959cad221c7e4478218d2
gggllm <515815530@qq.com>
2018-12-14 16:52:21 -0500

    filter by tags function

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -1,3 +1,13 @@
 module ActivitiesHelper
-
+  def get_activities params
+    page = params[:page].to_i
+    size = params[:size].to_i
+    if params[:activity_type_ids].blank?
+      activities = Activity.all
+    else
+      activities = Activity.where(activity_type_id: params[:activity_type_ids].split(","))
+    end
+    activities.order(updated_at: :desc)
+        .offset((page - 1) * size).limit(size)
+  end
 end
