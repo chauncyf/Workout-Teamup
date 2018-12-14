16c30a1c11f96a4755a39cdff5d3fb1317c30a73
gggllm <515815530@qq.com>
2018-12-14 17:13:17 -0500

    desc

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -7,7 +7,7 @@ module ActivitiesHelper
     else
       activities = Activity.where(activity_type_id: params[:activity_type_ids].split(","))
     end
-    activities.order(updated_at: :asc)
+    activities.order(updated_at: :desc)
         .offset((page - 1) * size).limit(size)
   end
 end
