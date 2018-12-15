54df1ee3e992aeb5836c39a5a931d05211b732b0
gggllm <515815530@qq.com>
2018-12-15 15:41:54 -0500

    using followed relationship

--- app/helpers/activities_helper.rb
+++ app/helpers/activities_helper.rb
@@ -6,20 +6,13 @@ module ActivitiesHelper
     if params[:activity_type_ids].blank?
       activities = Activity.all
     else
-        activities = Activity.where(activity_type_id: params[:activity_type_ids].split(","))
+      activities = Activity.where(activity_type_id: params[:activity_type_ids].split(","))
     end
 
     if show_follows == 1
-      activities = activities.where(starter_id: Follow.where(follower_id: current_user_id).pluck(:followee_id))
+      activities = activities.where(starter_id: current_user.followed)
     end
 
-=begin
-    if params[:activity_type_ids].blank?
-      activities = Activity.all
-    else
-      activities = Activity.where(activity_type_id: params[:activity_type_ids].split(","))
-    endless
-=end
     activities.order(updated_at: :desc)
         .offset((page - 1) * size).limit(size)
   end
