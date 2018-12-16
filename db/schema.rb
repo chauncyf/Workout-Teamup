3f3712872953c37a8938ff79798b8e6bca1a0ca5
irenezxr <xiangranzhao@outlook.com>
2018-12-16 13:49:27 -0500

    add activity end time; need to migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_14_170248) do
+ActiveRecord::Schema.define(version: 2018_12_16_184642) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,7 @@ ActiveRecord::Schema.define(version: 2018_12_14_170248) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "activity_type_id"
+    t.datetime "activity_end_time"
   end
 
   create_table "activity_contents", force: :cascade do |t|
